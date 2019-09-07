26b54c31170aa708729753f924211271c6aa4664
Adam Lohr <masterpi314@gmail.com>
2019-09-07 16:29:23 -0700

    CodePen Import

---
+++ script.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = canvas.scrollWidth * 2;
+canvas.height = canvas.scrollHeight * 2;
+canvas.addEventListener("touchstart", update);
+canvas.addEventListener("touchmove", update);
+canvas.addEventListener("touchend", update);
+var touchCache = [];
+var timeout = null;
+var winner = null;
+var waitStart = null;
+function update(e) {
+  touchCache = e.touches;
+  if (e.type == "touchstart" || e.type == "touchend" || e.type == "touchcancel") {
+    winner = null;
+    timeout !== null && clearTimeout(timeout);
+    waitStart = new Date();
+    timeout = setTimeout(function() {
+      if (winner !== null || !touchCache.length) return;
+      t = touchCache[Math.floor(Math.random() * touchCache.length)];
+      winner = t.identifier;
+    }, 2000);
+  }
+   e.preventDefault();
+}
+function draw() {
+  ctx.fillStyle="black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (t of touchCache) {
+    ctx.fillStyle=t.identifier == winner ? "blue": "red";
+    ctx.beginPath();
+    ctx.arc(t.clientX*2, t.clientY*2, 35*2, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 2*2;
+    ctx.beginPath();
+    ctx.arc(t.clientX*2, t.clientY*2, 45*2, Math.PI * -0.5, Math.PI * -0.5 + Math.PI * ((new Date()) - waitStart) / 1000);
+    ctx.stroke();
+  }
+}
+window.setInterval(draw, 10);
